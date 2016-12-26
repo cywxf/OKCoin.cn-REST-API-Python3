7694bcdc918018d728ec4fe44a61c6989aa5b4fc
Thibaut C <castaings.t@gmail.com>
2016-12-26 13:51:51 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='okcoincnapi',
+      version='1.0',
+      author='Nobody',
+      author_email='nobody@nowhere.no',
+      url='nowhere.no',
+      packages=['okcoincnapi'],
+      package_dir={'okcoincnapi': 'src/'}
+     )
